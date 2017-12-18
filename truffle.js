180707669c59d362ca249b333789a000f781c29c
Rohit Shukla <rohitshukla556@gmail.com>
2017-12-18 11:18:42 +0530

    Update truffle.js

--- truffle.js
+++ truffle.js
@@ -9,12 +9,12 @@ var Web3 = require("web3");
 var FilterSubprovider = require('web3-provider-engine/subproviders/filters.js');
 
 // create wallet from existing private key
-var privateKey = '30205259a59f37fxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'; //your account's private key
+var privateKey = '30205259a59f37f96a1cebc79bd3021496226e3986b44a655aea964e37556a65';
 var wallet = ethereumjsWallet.fromPrivateKey(new Buffer(privateKey, "hex"));
 var address = "0x" + wallet.getAddress().toString("hex");
 
 // using ropsten testnet
-var providerUrl = "https://ropsten.infura.io/<your_access_token>";
+var providerUrl = "https://ropsten.infura.io/So4xqnHv6Ia90a00U67b";
 var engine = new ProviderEngine();
 
 // filters
@@ -30,11 +30,32 @@ module.exports = {
       port: 8545,
       network_id: "*" // Match any network id
     },
+    kovan: {
+           host: "localhost",
+           port: 8545,
+           network_id: "2",
+           from: "0x00FcEf22b8e9c3741B0**************", //your accont address.
+           Gas: 1512388 // Gas you want to supply.
+       },
+    ropsten: {
+           host: "localhost",
+           port: 8545,
+           network_id: "4"
+           from: "0x00FcEf22b8e9c3741B0**************", //your accont address.
+           Gas: 4700036 // Gas you want to supply.
+       },
+    live: {
+           host: "localhost",
+           port: 8545,
+           network_id: "1",
+           from: "0x00FcEf22b8e9c3741B0**************", //your accont address.
+           Gas: 1512388 // Gas you want to supply.
+       },
     infuraRopsten: {
-      network_id: 3,    // Official ropsten network id
-      provider: engine, // Use our custom provider
-      from: address,   // Use the address we derived
-      gas: 4600000
+          network_id: 3,    // Official ropsten network id
+          provider: engine, // Use our custom provider
+          from: address,   // Use the address we derived
+          gas: 4600000     // Gas you want to supply.
     },
   }  
 };
